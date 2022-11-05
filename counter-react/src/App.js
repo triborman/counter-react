fcf63772817f523c9e35c7070a192ccb62f55f29
Tri Börman <wittyangel_98@hotmail.com>
2022-11-05 21:35:58 +0200

    final commit

---
+++ counter-react/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Button from "./components/Button";
+import "./App.css";
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="app">
+      <div>
+        <div class="count">
+          <h1>{count}</h1>
+        </div>
+        <div class="buttons">
+          <Button title={"-"} action={decrementCount} />
+          <Button title={"+"} action={incrementCount} />
+        </div>
+      </div>
+    </div>
+  );
+}
